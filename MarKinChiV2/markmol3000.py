2ba2a65d5e5a0ee0dd29cd693b4ead62703aab8b
Alex Lapsley <31190143+a-lapsley@users.noreply.github.com>
2023-10-09 09:38:02 +0100

    added stuff

--- MarKinChiV2/markmol3000.py
+++ MarKinChiV2/markmol3000.py
@@ -1,6 +1,8 @@
 class MarkMol3000(object):
     def __init__(self) -> None:
         self.molfile_lines = [] #Lines of v3000 .mol file that will be converted
+        self.core = None #RDKit Mol object containing core of molecule
+        self.rgroups = None #List of RDKit Mol objects containing each R group
 
     def load_from_file(self, file_path):
         # Loads V3000 molfile with path 'file_path'
@@ -29,10 +31,31 @@ class MarkMol3000(object):
             print("Unable to open file \'%s\'" % file_path)
             return -1
 
+    def generate_markinchi(self):
+        #Generates the MarkInChI for the loaded v3000 .mol file
+
+        self.core = self.generate_core()
+        self.rgroups = self.generate_rgroups()
+
+        pass
+
+    def generate_core(self):
+        #Generates RDKit Mol object for core of molecule from molfile_lines
+
+        pass
+
+    def generate_rgroups(self):
+        #Generates RDKit Mol object for each R group from molfile_lines and 
+        #adds them to the list rgroups
+        
+        pass
+
+
 if __name__ == "__main__":
     #If file run independently
     markinchi = MarkMol3000()
-    filedir = "C:\\Users\\alexl\\OneDrive\\Documents\\InChiMarkushTesting\\MarKinChiV2\\marvin\\test2.mol"
-    markinchi.load_from_file(filedir)
+    laptop_filedir = "C:\\Users\\alexl\\OneDrive\\Documents\\InChiMarkushTesting\\MarKinChiV2\\marvin\\test2.mol"
+    desktop_filedir = "D:\\alexl\\Documents\\InChiMarkushTesting\\MarKinChiV2\\marvin\\test2.mol"
+    markinchi.load_from_file(desktop_filedir)
     print(markinchi.molfile_lines)
     
